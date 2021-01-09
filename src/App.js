4d06e63ae4407e19426b283234b8dbcf5634c0e4
Łukasz Wójcik <lwojcik@lukem.net>
2021-01-09 18:51:05 +0100

    Tridi in CRA

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useEffect } from 'react';
+import * as Tridi from 'tridi';
 import './App.css';
+import './tridi.css';
 
 function App() {
+  const tridiInstance = Tridi.create({
+    element: '.tridi-basic',
+    location: '/images/white',
+    format: 'jpg',
+    count: 36,
+  });
+
+  useEffect(() => {
+    tridiInstance.load();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Lorem ipsum</p>
+        <div className="tridi-basic"></div>
       </header>
     </div>
   );
